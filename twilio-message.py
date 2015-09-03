be311dff74a5e9b6bb258b0b2814b7e8ff259c3c
Sofia Nguy <sofianguy@gmail.com>
2015-09-02 21:18:06 -0700

    Cleaned up twilio-message.py

--- twilio-message.py
+++ twilio-message.py
@@ -10,11 +10,3 @@ account_sid = environ['TWILIO_ACCOUNT_SID']
 auth_token  = environ['TWILIO_AUTH_TOKEN']
 
 client = TwilioRestClient(account_sid, auth_token)
-
-TWILIO_NUMBER = ""
-to_number = ""
-
-message = client.messages.create(from_ = TWILIO_NUMBER, 
-	to = to_number, body="HIIIIIIIII")
-
-print message.sid
